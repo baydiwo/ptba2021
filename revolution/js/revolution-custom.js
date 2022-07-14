1d1a7fbf0de37eca63df6e67caa8c641ef1352f3
Bayu Adi Wibowo <baydiwo@gmail.com>
2022-07-14 20:19:44 +0700

    new home page 3 section ready

--- revolution/js/revolution-custom.js
+++ revolution/js/revolution-custom.js
@@ -92,7 +92,7 @@ NOTE: This file includes all revolution slider scripts.
 							},
 							thumbnails: {
 								style: "erinyen",
-								enable: true,
+								enable: false,
 								width: 450,
 								height: 110,
 								min_width: 200,
@@ -1560,6 +1560,122 @@ var revapi292,
             },
             shadow:0,
             spinner:"spinner4",
+            stopLoop:"off",
+            stopAfterLoops:-1,
+            stopAtSlide:-1,
+            shuffle:"off",
+            autoHeight:"off",
+            disableProgressBar:"on",
+            hideThumbsOnMobile:"off",
+            hideSliderAtLimit:0,
+            hideCaptionAtLimit:0,
+            hideAllCaptionAtLilmit:0,
+            debugMode:false,
+            fallbacks: {
+              simplifyAll:"off",
+              nextSlideOnWindowFocus:"off",
+              disableFocusListener:false,
+            }
+          });
+        }
+      });
+
+      /*************************************
+ 		slider 01
+ **************************************/
+ var revapi2022,
+     tpj=jQuery;
+      tpj(document).ready(function() {
+        if(tpj("#rev_slider_1").revolution == undefined){
+          revslider_showDoubleJqueryError("#rev_slider_1");
+        }else{
+          revapi2022 = tpj("#rev_slider_1").show().revolution({
+            sliderType:"standard",
+            sliderLayout:"fullscreen",
+            dottedOverlay:"none",
+            delay:6000,
+            // navigation: {
+            //   keyboardNavigation:"off",
+            //   keyboard_direction: "horizontal",
+            //   mouseScrollNavigation:"off",
+            //   mouseScrollReverse:"default",
+            //   onHoverStop:"off",
+            //   touch:{
+            //     touchenabled:"on",
+            //     touchOnDesktop:"off",
+            //     swipe_threshold: 75,
+            //     swipe_min_touches: 1,
+            //     swipe_direction: "horizontal",
+            //     drag_block_vertical: false
+            //   }
+            //   ,
+            //   arrows: {
+            //     style:"zeus",
+            //     enable:true,
+            //     hide_onmobile:true,
+            //     hide_under:767,
+            //     hide_onleave:true,
+            //     hide_delay:200,
+            //     hide_delay_mobile:0,
+            //     tmp:'<div class="tp-title-wrap">    <div class="tp-arr-imgholder"></div> </div>',
+            //     left: {
+            //       h_align:"left",
+            //       v_align:"center",
+            //       h_offset:20,
+            //       v_offset:0
+            //     },
+            //     right: {
+            //       h_align:"right",
+            //       v_align:"center",
+            //       h_offset:20,
+            //       v_offset:0
+            //     }
+            //   }
+            // },
+            navigation: {
+							keyboardNavigation: "off",
+							keyboard_direction: "horizontal",
+							mouseScrollNavigation: "off",
+							mouseScrollReverse: "default",
+							onHoverStop: "off",
+							arrows: {
+								style: "metis",
+								enable: false,
+								hide_onmobile: true,
+								hide_under: 768,
+                hide_over: 1000,
+								hide_onleave: false,
+								tmp: '',
+								left: {
+									h_align: "left",
+									v_align: "center",
+									h_offset: 0,
+									v_offset: 0
+								},
+								right: {
+									h_align: "right",
+									v_align: "center",
+									h_offset: 0,
+									v_offset: 0
+								}
+							},
+							thumbnails: {
+								enable: false,
+							},
+							touch: {
+								touchenabled: 'on',
+								swipe_threshold: 75,
+								swipe_min_touches: 1,
+								swipe_direction: 'horizontal',
+								drag_block_vertical: true
+							}
+						},
+            visibilityLevels:[1440,1024,778,480],
+            gridwidth:1900,
+            gridheight:850,
+            lazyType:"none",
+            shadow:0,
+            spinner:"spinner2",
             stopLoop:"off",
             stopAfterLoops:-1,
             stopAtSlide:-1,
